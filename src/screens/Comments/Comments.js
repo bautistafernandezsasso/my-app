6bd0850ab6f815340b833a7e98e7c4748ee902a4
santiagochamo <schamo@udesa.edu.ar>
2022-11-15 13:42:39 -0300

    Comments y CreatePost basico

---
+++ src/screens/Comments/Comments.js
@@ -0,0 +1,15 @@
+import { Text, View, FlatList } from 'react-native'
+import React, { Component } from 'react'
+
+class Comments extends Component {
+    constructor(){
+        super()
+        this.state={
+            info: comments
+        }
+    }
+  
+   
+}
+
+export default Comments
---
+++ src/screens/CreatePost/CreatePost.js
@@ -0,0 +1,15 @@
+import { Text, View, FlatList } from 'react-native'
+import React, { Component } from 'react'
+
+class CreatePost extends Component {
+    constructor(){
+        super()
+        this.state={
+            
+        }
+    }
+  
+    
+}
+
+export default CreatePost

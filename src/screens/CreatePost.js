e357302f51da61e0f17e4ac8aad8ea0cea0a5130
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-11-24 11:59:48 -0300

    .

--- src/screens/CreatePost.js
+++ src/screens/CreatePost.js
@@ -50,7 +50,7 @@ class CreatePost extends Component {
         {
             this.state.mostrarCamara ?
             <Camara
-            subirPosteo={(url)=> this.onImageUpload(url)}
+            subirPosteo={(url)=> this.props.onImageUpload(url)}
             /> :
             <>
                 <TextInput

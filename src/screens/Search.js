8d5a657abecf99fc2b9fbe65926b9f629364e583
Bauti Linares <bato@MacBook-Pro-de-Bauti.local>
2022-11-24 21:18:06 -0300

    css y arreglo

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,103 +1,67 @@
 import { Text, View, StyleSheet, TextInput, TouchableOpacity,  FlatList, } from 'react-native'
 import React, { Component } from 'react'
 import { auth, db } from '../firebase/config'
-import { SearchBar } from 'react-native'
+
 
 class Search extends Component {
     constructor(props){
         super(props)
         this.state={
-          data: [],
-          id:'', 
-          results: [],
-          users: [], 
-          loading: false,
-          busqueda: '',
-        }
-    }
+          text:'',
+          posts: [],
+      }
+  };
 
-    componentDidMount(){
-        db.collection('users')
-        .onSnapshot(doc => {
-          let resultados = [];
-          doc.forEach(doc => {
-            resultados.push({
-                id: doc.id, 
-                data: doc.data()
-            })
-            
+
+
+buscar(text){
+
+  this.setState({text:text})
+  console.log(text)
+  db.collection('users').where('owner', '==', text).onSnapshot(
+      docs => {
+          let posts = [];
+          docs.forEach( doc => {
+              posts.push({
+                  id: doc.id,
+                  data: doc.data()
+              })
+              this.setState({
+                  posts: posts,
+              }, console.log(this.state.posts))
           })
-          this.setState(
-            {users: resultados}
-          )
-         
-        })
-    }
-
-
-
-    buscar(text){
-    
-        let usersFilter = this.state.data.filter(elm =>
-        { 
-         return elm.data.username.toUpperCase().includes(text.toUpperCase())})
-         console.log(usersFilter);
-         this.setState({
-           results: usersFilter,
-           busqueda: text,
-        })
-    }
-
-   render() {
-  
-    return( 
-        <View style={styles.container}>
-            <Text style={styles.text}>Search</Text>
-            <TextInput  
-              onChangeText={ text => this.buscar( text )}
-              placeholder='Ingresa tu busqueda'
-              value={this.state.busqueda}>
-            </TextInput>
-
-    
-    {
-        <FlatList style={styles.text}
-          data={this.state.users}
-          keyExtractor={(item) => item.id}
-          renderItem= {({item}) => <Text>{item.data.username}</Text>}
-         />
-    }
-             
-        </View>
-    )
-  }
+          
+      }
+  )
 }
 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems: 'center',
-    backgroundColor: 'rgb(16, 146, 201)',
-    paddingHorizontal:32
-},
-title:{
-    fontSize:30  
-},
-text:{
-    backgroundColor: 'rgba(204, 204 ,204, 0.1)',
-    fontSize: 20,
-    borderRadius: 10,
-    marginTop: 5
- 
-},
-text2:{
-    backgroundColor: 'rgb(134, 192, 217)',
-    borderRadius: 10,
-    fontSize: 30,
-    marginTop: 5
- 
-},
-})
+
+render(){
+  return( <View> 
+      <TextInput 
+          placeholder='Buscar usuarios..'
+          keyboardType="default"
+          onChangeText={text => this.buscar(text)}
+          value={this.state.text}/>
+          <TouchableOpacity onPress={()=>this.buscar(this.state.text)}>
+                      <Text>Buscar</Text>
+          </TouchableOpacity>
+          <FlatList 
+                      data={this.state.posts}
+                      keyExtractor={ onePost => onePost.id.toString()}
+                      renderItem={ ({item})  => <TouchableOpacity onPress={()=>this.props.navigation.navigate('Profile',{ email: item.data.owner } )}>
+                      { this.state.text === item.data.owner ?
+              <Text> {item.data.owner}</Text> : <Text>No exsiste el usuario</Text> }
+                 </TouchableOpacity> }
+                          />
+                  
+      </View>
+      )
+
+}
+
+
+
+}
 
 export default Search;
